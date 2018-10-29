660ec28383be798e6c758fd4eb71d7af3e4ddef0
r.sato <sugarhorn0929@gmail.com>
2018-10-29 20:46:18 +0900

    [add] make log file and outputs in "pa10_log.csv"

--- script/pa10_logger.py
+++ script/pa10_logger.py
@@ -7,10 +7,36 @@ import csv
 
 class PA10Logger(object):
     def __init__(self):
-        # rospy.init_node(name='pa10_logger_node')
+        rospy.init_node(name='pa10_logger_node')
+        self.listener = tf.TransformListener()
+        self.loop_rate = rospy.Rate(hz=10)
+
         self.file = open('pa10_log.csv', 'w')
-        # self.listener = tf.TransformListener()
+        self.writer = csv.writer(self.file, lineterminator='\n')
+
+    def getPA10Status(self):
+        self.listener.waitForTransform(
+            target_frame='world', source_frame='link7', time=rospy.Time(), timeout=rospy.Duration(1.0))
+        (trans, rot) = self.listener.lookupTransform(
+            target_frame='world', source_frame='link7', time=rospy.Time(secs=0))
+        r, p, y = tf.transformations.euler_from_quaternion(rot)
+
+        line = [trans[0], trans[1], trans[2], r, p, y]
+        self.writer.writerow(line)
+
+    def fileClose(self):
+        self.file.close()
 
 
 if __name__ == "__main__":
-    pa10 = PA10Logger()
+    logger = PA10Logger()
+
+    while not rospy.is_shutdown():
+        try:
+            logger.getPA10Status()
+            logger.loop_rate.sleep()
+
+        except KeyboardInterrupt:
+            continue
+
+    logger.fileClose()
